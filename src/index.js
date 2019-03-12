15113dad879767b5fc6b58d7cac61e1d4701c31b
SAMOVENDYUK NIKOLAY <samovnick@gmail.com>
2019-03-12 21:07:26 +0300

    Complete love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    //walk to path
+    let first, second, third;
+    first = i + 1;
+    second = preferences[first - 1];
+    if (second == first) continue;
+    third = preferences[second - 1];
+    if (third == second) continue;
+    if (preferences[third - 1] == first) {
+      count++;
+    }
+  }
+
+  return count / 3;
 };
